983e0fb33f8ea3bfb50c14d2aea6416b78e2b5a4
AlexPrudius <prudius.av@ya.ru>
2023-09-18 20:32:29 +0500

    Added products array

--- project-react/src/App.tsx
+++ project-react/src/App.tsx
@@ -1,12 +1,30 @@
-import React, {createElement as e, useState} from 'react';
+import React, {createElement as e, useEffect, useState} from 'react';
 import {Product} from './components/Product'
-import {products} from './data/products'
+//import {products} from './data/products'
+import axios from 'axios';
+import { IProduct } from './models';
 
 function App() {
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  async function fetchProducts() {
+    const response = await axios.get<IProduct[]> ('https://fakestoreapi.com/products?limit=5')
+    setProducts(response.data)
+  }
+
+useEffect( () => {
+  fetchProducts()
+}, [])
+
+
   return (
     <div className="container mx-auto max-w-2xl pt-5">
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+
+      { products.map(product => <Product product={product} key={product.id}/>)}
+
+
+      {/* <Product product={products[0]} />
+      <Product product={products[1]} /> */}
     </div>
   )
 }
--- project-react/src/components/Product.tsx
+++ project-react/src/components/Product.tsx
@@ -8,6 +8,10 @@ interface ProductProps {
 export function Product({product}: ProductProps) {
     const[details, setDetails] = useState(false)
 
+    const btnBgClassName = details ? 'bg-yellow-400' : 'bg-blue-400'
+
+    const btnClasses = ['py-2 px-4 border', btnBgClassName]
+
 return(
         <div 
         className="border py-2 px-4 rounded flex flex-col items-center mb-2"
@@ -16,23 +20,16 @@ return(
             <p> {product.title} </p>
             <p className="font-bold"> {product.price} </p>
             <button 
-            className= "py-2 px-4 border bg-yellow-400"
-            onClick={() => setDetails(true)}
+                className= {btnClasses.join(' ')}
+                onClick={() => setDetails(prev => !prev)}
 
             >
-                Show Details 
-            </button>
-
-            <button 
-            className= "py-2 px-4 border bg-blue-400"
-            onClick={() => setDetails(false)}
-
-            >
-                Hide Details 
+                {details ? 'Hide Details' : 'Show Details'} 
             </button>
 
             {details && <div>
                 <p> {product.description} </p>
+                <p>Rate: <span style={{fontWeight: 'bold'}} >{product.rating.rate}</span></p>
             </div>}
         </div>
     )
